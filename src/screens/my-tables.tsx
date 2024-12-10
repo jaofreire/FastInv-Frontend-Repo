62d36e197783d3a7400ae707e1ca86bae865187a
jaofreire <joao.gabriel18872@gmail.com>
2024-12-10 15:34:05 -0300

    feat: adicionando botão de criar nova tabela e migrar tabela Excel

--- src/screens/my-tables.tsx
+++ src/screens/my-tables.tsx
@@ -1,6 +1,8 @@
 import SideBar from "@/components/Global/sidebar";
 import TableCard from "@/components/my-tables/table-card";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { BadgePlus, Blend, ClipboardPaste } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -16,8 +18,16 @@ function MyTables() {
                     <div className="w-full h-full flex p-6">
                         <Card className="w-full">
                             <div className="flex flex-col h-full">
-                                <CardHeader className="mb-4 h-28">
-                                    <CardTitle className="pt-5 pl-10 text-3xl font-bold">Suas <span className="text-orange-400">Tabelas</span>:</CardTitle>
+                                <CardHeader className="flex mb-5 w-full h-28 ">
+                                    <div className="flex w-full h-full">
+                                        <div className="w-full">
+                                            <CardTitle className="pt-5 pl-10 text-3xl font-bold">Suas <span className="text-orange-400">Tabelas</span>:</CardTitle>
+                                        </div>
+                                        <div className="flex flex-col gap-3 justify-end h-24 w-52 pr-5">
+                                            <Button className="w-30 h-8 bg-orange-500 opacity-90 text-black font-semibold hover:opacity-100 hover:bg-orange-500"><BadgePlus/>Criar tabela</Button>
+                                            <Button className="w-30 h-8 bg-slate-950 opacity-90 text-white font-semibold hover:opacity-100 hover:bg-slate-950"><Blend/>Migrar tabela Excel</Button>
+                                        </div>
+                                    </div>
                                 </CardHeader>
                                 <CardContent className="flex flex-col items-center gap-5 w-full">
                                     {tables.map((value, index) => (
