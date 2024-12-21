2c0bfa5b09258b779bce0795d1738880d85d2cec
jaofreire <joao.gabriel18872@gmail.com>
2024-12-21 13:13:07 -0300

    Integrando chamada GET para buscar o movement history relacionado ao companyId, alterando tabela de listagem do historico de movimentação

---
+++ src/api/api-movement-history.ts
@@ -0,0 +1,13 @@
+import { ApiResponse } from "@/types/api-response-types/api-response"
+import api from "./api"
+import { MovementHistoryType } from "@/types/api-response-types/movement-history/movement-history-type"
+
+export const fetchMovementsHistoryByCompanyId = async (companyId: string): Promise<MovementHistoryType[]> => {
+    const data = await api.get<ApiResponse<MovementHistoryType>>('/MovementHistoryEvent/company/' + companyId)
+        .then((response) => {
+            const responseData = response.data;
+            return responseData.responseList;
+        });
+
+    return data;
+}
